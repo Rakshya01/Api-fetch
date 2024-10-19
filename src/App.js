c57af4b65373b9f973f8539404b89ae9df54a65f
Rakshya <kunwarrakshya20@gmail.com>
2024-10-19 22:43:38 +0545

    fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import logo from './logo.svg';
+import Category from "./Category";
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  let [finalCategory, setFinalCategory] = useState([])
+  let [finalPro, setFinalProduct]= useState([])
+  let [catName,setCatname] = useState('')
+
+  let getCategory = () => {
+    axios.get('https://dummyjson.com/products/category-list')
+      .then((res) => res.data)
+      .then((finalRes) => {
+        setFinalCategory(finalRes)
+      })
+  }
+
+  let getProducts = ()=>{
+    axios.get('https://dummyjson.com/products')
+    .then((proRes)=>proRes.data)
+    .then((finalRes)=>{
+      setFinalProduct(finalRes.products)
+    })
+  }
+
+  useEffect(() => {
+    getCategory();
+    getProducts();
+  }, [])
+
+  useEffect(()=>{
+    if(catName!==""){
+      axios.get(`https://dummyjson.com/products/category/${catName}`)
+      .then((proRes)=>proRes.data)
+      .then((finalRes)=>{
+        setFinalProduct(finalRes.products)
+      })
+    }
+    
+
+  },[catName])
+
+  let Pitems = finalPro.map((products,index)=>{
+    return(
+      <ProductItems key={index} pdata={products}/>
+    )
+    
+    
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='py-[40px]'>
+        <div className='max-w-[1320px] mx-auto'>
+          <h1 className="text-center text-[40px] font-bold mb-[30px]">Our Products</h1>
+          <div className="grid grid-cols-[30%_auto] gap-[20px]">
+            <div>
+              <Category finalCategory={finalCategory} setCatname={setCatname} />
+            </div>
+            <div>
+              <div className="grid grid-cols-3 gap-5 ">
+
+               {Pitems}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+function ProductItems({pdata}) {
+  console.log(pdata)
+  return (
+    <div className="shadow-lg text-center pb-4">
+      <img src={pdata.thumbnail} className="w-[100%] h-[220px]"/>
+      <h4>{pdata.title}</h4>
+      <b>Rs {pdata.price}</b>
+    </div>
+  )
+}
